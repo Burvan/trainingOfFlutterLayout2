e3b13cb98a1773661cb3aac888396f07e58af1ad
Burvan <burvani@mail.ru>
2023-02-26 14:28:13 +0300

    fetch: Main profile information

--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -1,82 +1,144 @@
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatelessWidget {
-  const UserProfile({Key? key}) : super(key: key);
+  UserProfile({Key? key}) : super(key: key);
+
+  final List<ProfileColumnData> profileColumn = [
+    ProfileColumnData('6', 'Posts'),
+    ProfileColumnData('270', 'Followers'),
+    ProfileColumnData('163', 'Following'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _MyAppBar(),
-      body: Container(
-        width: double.infinity,
-        color: Colors.black,
-        child: ListView(
-          physics: BouncingScrollPhysics(),
-          children: [
-            Column(
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          Container(
+            width: double.infinity,
+            color: Colors.black,
+            child: Row(
               children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 15),
-                      child: Container(
-                        width: 80,
-                        height: 80,
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage('assets/avatar.jpg'),
-                        ),
-                      ),
-                    ),
-                    Column(
-                      children: const [
-                        Text(
-                          '6',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        Text(
-                          'Публикаций',
-                          style: TextStyle(
-                              color: Colors.white,  fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      children: const [
-                        Text(
-                          '271',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        Text(
-                          'Подписчи...',
-                          style: TextStyle(
-                              color: Colors.white,  fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      children: const [
-                        Text(
-                          '163',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        Text(
-                          'Подписки',
-                          style: TextStyle(
-                              color: Colors.white,  fontSize: 15),
-                        ),
-                      ],
-                    )
-                  ],
-                )
+                _AvatarWidget(),
+                _ProfileWidget(profileColumn: profileColumn),
               ],
-            )
+            ),
+          ),
+          _MainInformationWidget()
+        ],
+      ),
+    );
+  }
+}
+
+class _MainInformationWidget extends StatelessWidget {
+  const _MainInformationWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //width: double.infinity,
+      color: Colors.black,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, bottom: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Илья Бурван',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15),
+            ),
+            SizedBox(height: 3),
+            Text(
+              'Athlete',
+              style: TextStyle(color: Colors.grey, fontSize: 14),
+            ),
+            SizedBox(height: 3),
+            Text(
+              'Просто сбился с пути...',
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _AvatarWidget extends StatelessWidget {
+  const _AvatarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+          top: 10, bottom: 10, right: 30, left: 25
+      ),
+      child: Container(
+        width: 80,
+        height: 80,
+        child: const CircleAvatar(
+          backgroundImage: AssetImage('assets/avatar.jpg'),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileColumnData {
+  final String textOne;
+  final String textTwo;
+
+  ProfileColumnData(this.textOne, this.textTwo);
+}
+
+class _ProfileWidget extends StatelessWidget {
+  final List<ProfileColumnData> profileColumn;
+  const _ProfileWidget({Key? key, required this.profileColumn})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      //mainAxisSize: MainAxisSize.max,
+      //mainAxisAlignment: MainAxisAlignment.end,
+      children: profileColumn
+          .map((data) => _ProfileColumnWidget(data: data))
+          .toList(),
+    );
+  }
+}
+
+class _ProfileColumnWidget extends StatelessWidget {
+  final ProfileColumnData data;
+  const _ProfileColumnWidget({Key? key, required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 25.0),
+      child: Column(
+        //crossAxisAlignment: CrossAxisAlignment.,
+        children: [
+          Text(
+            data.textOne,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15),
+          ),
+          Text(
+            data.textTwo,
+            style: const TextStyle(color: Colors.white, fontSize: 15),
+          ),
+        ],
       ),
     );
   }
@@ -114,17 +176,3 @@ class _MyAppBar extends StatelessWidget with PreferredSizeWidget {
   }
 }
 
-// actions: const <Widget>[
-// Expanded(
-// child: Text(
-// '_burbon___',
-// style: TextStyle(
-// color: Colors.white,
-// fontWeight: FontWeight.bold
-// ),
-// textAlign: TextAlign.center,
-// ),
-// ),
-// Icon(Icons.add_box_outlined, color: Colors.white),
-// Icon(Icons.menu, color: Colors.white,)
-// ],
